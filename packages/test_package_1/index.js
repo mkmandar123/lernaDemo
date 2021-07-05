531cda7b6f158bb1ee57c90dc0ff322ebb57283a
Mandar <mkmandar123@gmail.com>
2021-07-05 16:22:44 +0530

    checking tests running for single package

--- packages/test_package_1/index.js
+++ packages/test_package_1/index.js
@@ -1,4 +1,4 @@
 exports.setupCloudFunctions = function () {
-    console.log('>>>>>>>>>setting up cloud function testFunction');
+    console.log('>>>>>>>>>setting up cloud function testFunction!');
     Parse.Cloud.define('testFunction', () => 'function working from package!!');
 };
